5220c7cc1f9c60099dae716bedb9fad69de388d3
Maringmei (Globizs) <maringmei@globizs.com>
2025-10-05 14:34:00 +0000

    make SidebarTrigger and UserNav space between

--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -4,13 +4,12 @@ import { UserNav } from './user-nav';
 
 export default function AppHeader() {
   return (
-    <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm md:px-6">
+    <header className="sticky top-0 z-10 flex h-16 items-center justify-between gap-4 border-b bg-background/80 px-4 backdrop-blur-sm md:px-6">
       <div className="flex items-center gap-2">
-         <SidebarTrigger />
+        <SidebarTrigger />
       </div>
-     
-      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        
+
+      <div className="flex items-center gap-4 md:gap-2 lg:gap-4">
         <UserNav />
       </div>
     </header>
