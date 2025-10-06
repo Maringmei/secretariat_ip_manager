3073382cb37fd00caae577ada5442ab7e200ccca
Maringmei (Globizs) <maringmei@globizs.com>
2025-10-06 06:03:51 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/components/my-requests-list-page.tsx
+++ src/components/my-requests-list-page.tsx
@@ -9,6 +9,7 @@ import { Loader2, Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { Label } from "./ui/label";
 
 interface MyRequestsListPageProps {
     title: string;
