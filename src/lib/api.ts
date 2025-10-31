466ae25fea5f3d2690298d618268bf8bb983568f
Maringmei (Globizs) <maringmei@globizs.com>
2025-10-31 11:24:56 +0000

    newe employee code uploaded added

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,3 @@
-const isStaging = true;
+const isStaging = false;
 
 export const API_BASE_URL = isStaging ? 'https://iprequeststagingapi.globizsapp.com/api' : 'https://iprequestapi.globizsapp.com/api'; 
