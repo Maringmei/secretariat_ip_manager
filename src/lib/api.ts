94d719d93c94808c64dc282083805ed3250a0987
Maringmei <mshengangkabui@gmail.com>
2025-12-15 11:40:16 +0530

    audit new baseUrl updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,4 @@
 const isStaging = true;
+const isAudit = true
 
-export const API_BASE_URL = isStaging ? 'https://iprequeststagingapi.globizsapp.com/api' : 'https://iprequestapi.globizsapp.com/api'; 
+export const API_BASE_URL = isAudit ? "https://criticalinfrastagingapi.globizsapp.com/api" : isStaging ? 'https://iprequeststagingapi.globizsapp.com/api' : 'https://iprequestapi.globizsapp.com/api'; 
